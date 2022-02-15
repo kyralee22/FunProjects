4f28445a7bab9a1d7838c4713380019b0333c645
victoriapuck15 <victoriapuck15@gmail.com>
2022-02-15 12:26:15 -0500

    made the line pink :p

--- hotcrazymatrix.py
+++ hotcrazymatrix.py
@@ -5,7 +5,7 @@ import numpy as np
 hot = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 crazy = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
-plt.plot(hot,crazy)
+plt.plot(hot,crazy,'m--')
 plt.title('HOT CRAZY MATRIX')
 plt.xlabel('HOT LEVEL')
 plt.ylabel('CRAZY LEVEL')
